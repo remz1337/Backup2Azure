98a41b226cc453aba375cf12089dac29eadabf2d
Rémi Bédard-Couture <remi.bc@outlook.com>
2024-04-18 16:01:59 -0400

    Initial script

---
+++ AzUpload.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+logfile="log.txt"
+
+echo "Uploading backups..." > $logfile
+
+account_name="XXX"
+container_name="XXX"
+
+sp_app_id="XXX"
+sp_password="XXX"
+sp_tenant_id="XXX"
+
+email="XXX"
+
+search_dir=/mnt/pve/backup/dump
+#file_pattern=vzdump-*.tar.zst
+#*.tar.zst for CT and pve host backups
+#*.vma.zst for VM backups
+pve_config_file_pattern=pve-host*.zst
+backups_file_pattern=vzdump*.zst
+#after=$(date -u --date="90 days ago" +"%Y-%m-%dT%H:%M:%SZ")
+tier="ARCHIVE" #Hot,Cold,Archive
+
+start=$(date +%s)
+
+#Token expires after 90 of inactivity. Use a different scheduled (monthly) job to keep login alive
+az login --service-principal -u ${sp_app_id}  -p ${sp_password} --tenant ${sp_tenant_id} > $logfile 2>&1
+
+#az storage blob upload-batch -d $container_name --account-name $account_name --auth-mode login -s $search_dir --pattern $file_pattern --tier $tier --overwrite false >> $logfile 2>&1
+
+echo "Starting upload of PVE host config..." >> $logfile
+{ # try
+    az storage blob upload-batch -d $container_name --account-name $account_name --auth-mode login -s $search_dir --pattern $pve_config_file_pattern --tier $tier --overwrite false >> $logfile 2>&1
+} || { # catch
+    echo "Exception while uploading PVE host config." >> $logfile
+}
+
+echo "Starting upload of backups..." >> $logfile
+{ # try
+    az storage blob upload-batch -d $container_name --account-name $account_name --auth-mode login -s $search_dir --pattern $backups_file_pattern --tier $tier --overwrite false >> $logfile 2>&1
+} || { # catch
+    echo "Exception while uploading backups." >> $logfile
+}
+
+end=$(date +%s)
+seconds=$(($end-$start))
+elapsed=$(date -d@$seconds -u +"%j days %H:%M:%S")
+
+body=$(cat <<-END
+Upload of homelab backups to cloud completed.
+Job time: $elapsed
+
+END
+)
+
+
+#Confirm upload
+mail -A $logfile -s "[Homelab] Offsite backup completed" "$email" <<< "$body"
+
+#rm $logfile
+
+echo "Done"
