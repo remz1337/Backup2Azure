3439f8048d16a6b3a2276e16dcde59cbbade1f89
Rémi Bédard-Couture <remi.bc@outlook.com>
2024-04-19 14:34:37 -0400

    Log to stdout; source variable from conf file

--- AzUpload.sh
+++ AzUpload.sh
@@ -25,6 +25,14 @@ function validate_env(){
     echo "Missing email environment variable"
     exit
   fi
+  if [ -z "$search_dir" ]; then
+    echo "Missing search_dir environment variable"
+    exit
+  fi
+  if [ -z "$tier" ]; then
+    echo "Missing tier environment variable"
+    exit
+  fi
 }
 
 
@@ -33,47 +41,47 @@ validate_env
 exit
 #WIP, haven't tested past this
 
-logfile="log.txt"
+logfile="/var/log/backup2azure.log"
 
-echo "Uploading backups..." > $logfile
+echo "Uploading backups..."
 
-account_name="XXX"
-container_name="XXX"
+# account_name="XXX"
+# container_name="XXX"
+# sp_app_id="XXX"
+# sp_password="XXX"
+# sp_tenant_id="XXX"
+# email="XXX"
+# #search_dir=/mnt/pve/backup/dump
+# search_dir="XXX"
+# tier="XXX" #HOT,COLD,ARCHIVE
 
-sp_app_id="XXX"
-sp_password="XXX"
-sp_tenant_id="XXX"
-
-email="XXX"
-
-search_dir=/mnt/pve/backup/dump
 #file_pattern=vzdump-*.tar.zst
 #*.tar.zst for CT and pve host backups
 #*.vma.zst for VM backups
 pve_config_file_pattern=pve-host*.zst
 backups_file_pattern=vzdump*.zst
 #after=$(date -u --date="90 days ago" +"%Y-%m-%dT%H:%M:%SZ")
-tier="ARCHIVE" #Hot,Cold,Archive
+
 
 start=$(date +%s)
 
 #Token expires after 90 of inactivity. Use a different scheduled (monthly) job to keep login alive
-az login --service-principal -u ${sp_app_id}  -p ${sp_password} --tenant ${sp_tenant_id} > $logfile 2>&1
+az login --service-principal -u ${sp_app_id}  -p ${sp_password} --tenant ${sp_tenant_id}
 
 #az storage blob upload-batch -d $container_name --account-name $account_name --auth-mode login -s $search_dir --pattern $file_pattern --tier $tier --overwrite false >> $logfile 2>&1
 
-echo "Starting upload of PVE host config..." >> $logfile
+echo "Starting upload of PVE host config..."
 { # try
-    az storage blob upload-batch -d $container_name --account-name $account_name --auth-mode login -s $search_dir --pattern $pve_config_file_pattern --tier $tier --overwrite false >> $logfile 2>&1
+    az storage blob upload-batch -d $container_name --account-name $account_name --auth-mode login -s $search_dir --pattern $pve_config_file_pattern --tier $tier --overwrite false
 } || { # catch
-    echo "Exception while uploading PVE host config." >> $logfile
+    echo "Exception while uploading PVE host config."
 }
 
-echo "Starting upload of backups..." >> $logfile
+echo "Starting upload of backups..."
 { # try
-    az storage blob upload-batch -d $container_name --account-name $account_name --auth-mode login -s $search_dir --pattern $backups_file_pattern --tier $tier --overwrite false >> $logfile 2>&1
+    az storage blob upload-batch -d $container_name --account-name $account_name --auth-mode login -s $search_dir --pattern $backups_file_pattern --tier $tier --overwrite false
 } || { # catch
-    echo "Exception while uploading backups." >> $logfile
+    echo "Exception while uploading backups."
 }
 
 end=$(date +%s)
