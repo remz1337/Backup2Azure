221011cd9253eae84e7baf63014bf5990795b858
Rémi Bédard-Couture <remi.bc@outlook.com>
2024-04-22 13:03:10 -0400

    When creating the service principal, pass the subscription id and storage ressource groupe as arguments

--- CreateServicePrincipal.sh
+++ CreateServicePrincipal.sh
@@ -15,10 +15,21 @@ fi
 
 
 sp_name="sp-blob-pve-archive"
-az_subscription_id="XXX"
-az_storage_rg="XXX"
+#az_subscription_id="XXX"
+#az_storage_rg="XXX"
 # account_name="XXX"
 # container_name="XXX"
+
+#Check if info has been passed as arguments
+if [ $# -ge 2 ] && [ ! -z "$1" ] && [ ! -z "$2" ]; then
+  az_subscription_id="$1"
+  echo "az_subscription_id has been set to $az_subscription_id"
+  az_storage_rg="$2"
+  echo "az_storage_rg has been set to $az_storage_rg"
+else
+  echo "Missing az_subscription_id and/or az_storage_rg. Pass them as arguments when calling this script."
+  exit
+fi
 
 az ad sp create-for-rbac --name $sp_name \
                          --role "Storage Blob Data Contributor" \
