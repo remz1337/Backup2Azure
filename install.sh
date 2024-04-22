14e6d837961943f057ef4d325e877406405f2237
remz1337 <remi.bc@outlook.com>
2024-04-22 11:12:30 -0400

    Add service to keep alive the Azure login token

--- install.sh
+++ install.sh
@@ -6,12 +6,11 @@ if [ ! -f "/etc/Backup2Azure/backup2azure.conf" ]; then
   cp backup2azure.conf /etc/Backup2Azure/
 fi
 
-#Create link to executable script
+#Copy executable script
 mkdir -p /opt/Backup2Azure
 cp AzUpload.sh /opt/Backup2Azure/AzUpload.sh
 
-
-#Create systemd timer
+#Create backup upload service
 cat <<EOF >/etc/systemd/system/backup2azure.timer
 [Unit]
 Description=Backup2Azure timer
@@ -19,19 +18,14 @@ Requires=backup2azure.service
 
 [Timer]
 Unit=backup2azure.service
-#Every minute
-#OnCalendar=*-*-* *:*:00
 #Every 3 months
 #OnCalendar=quarterly
-#OnCalendar=02,05,08,11-01 00:00:00
 OnCalendar=Mon 02,05,08,11-01..07 00:00:00
 
 [Install]
 WantedBy=timers.target
 EOF
 
-
-#Create systemd service
 cat <<EOF >/etc/systemd/system/backup2azure.service
 [Unit]
 Description=Backup2Azure service
@@ -49,9 +43,35 @@ StandardError=truncate:/var/log/backup2azure.log
 WantedBy=multi-user.target
 EOF
 
-#systemctl enable -q --now backup2azure
 systemctl enable -q --now backup2azure.timer
 
+#Installing keepalive service for login token
+cat <<EOF >/etc/systemd/system/azlogin.timer
+[Unit]
+Description=Azure Login keepalive timer
+Requires=azlogin.service
 
-echo "done"
-exit
+[Timer]
+Unit=azlogin.service
+OnCalendar=monthly
+
+[Install]
+WantedBy=timers.target
+EOF
+
+cat <<EOF >/etc/systemd/system/azlogin.service
+[Unit]
+Description=Azure Login keepalive service
+After=network.target
+Wants=azlogin.timer
+
+[Service]
+Type=oneshot
+EnvironmentFile=/etc/Backup2Azure/backup2azure.conf
+ExecStart=/bin/bash -c 'az login --service-principal -u \${sp_app_id}  -p \${sp_password} --tenant \${sp_tenant_id}'
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable -q --now azlogin.timer
